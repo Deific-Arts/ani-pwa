158cbb4081778b7493b1a59ee3936797a2adda4e
Hasani Rogers <hasani.rogers@gmail.com>
2025-08-08 19:59:10 -0400

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,30 @@ import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+let site;
+let port;
+
+switch (import.meta.env.PUBLIC_RUNTIME_ENVIRONMENT) {
+  case 'development':
+    port = 4323;
+    site = "https://dev.anibookquotes.com";
+    break;
+  case 'production':
+    port = 4324;
+    site = "https://anibookquotes.com";
+    break;
+  default:
+    port = 4322;
+    site = `http://localhost:${port}`;
+}
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hasanirogers.me',
+	site,
 	integrations: [mdx(), sitemap()],
   output: 'server',
   adapter: node({ mode: 'standalone' }),
   server: {
-    port: 4322
+    port
   }
 });
